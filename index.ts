2ddec6e988791e0aaa7c913b99e06cff13db7030
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-14 15:22:48 -0500

    Implement get All Recipes && .env added

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
 import 'dotenv/config'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const client = new MongoClient(process.env.MONGO_URI as string)
+const db = client.db('recipe-app')
+const recipes = db.collection('recipes')
+
+client.connect()
+console.log('connected to Mongo')
+
+
 const PORT = process.env.PORT
 
 app.listen(PORT , () => console.log('Database listening on port', PORT))
 
-app.get('/', (req, res) => {
-  res.json('Here is my API responding')
+// 7. Implement getAllRecipes......
+app.get('/',async (req, res) => {
+  const AllRecipes = await recipes.find().toArray()
+  res.send(AllRecipes)
 })
