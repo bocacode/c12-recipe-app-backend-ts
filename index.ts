14ec113b8fc1580c58f0065c68951724ffff7a0f
Bradley Kirks <spinnyorb23@aim.com>
2023-11-14 16:21:41 -0500

    added connection

--- index.ts
+++ index.ts
@@ -3,13 +3,19 @@ import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import 'dotenv/config'
 
+
+const client = new MongoClient(process.env.MONGO_URI as string)
+const db = client.db('recipe-app')
 const app = express()
-app.use(cors())
+const recipe = db.collection('recipe')
+
+client.connect()
+// const PORT = process.env.PORT
+
+
 app.use(express.json())
-
-const PORT = process.env.PORT
-
-app.listen(PORT , () => console.log('Database listening on port', PORT))
+app.use(cors())
+app.listen(process.env.PORT, () => console.log('Database listening on port',))
 
 app.get('/', (req, res) => {
   res.json('Here is my API responding')
