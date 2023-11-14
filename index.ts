a258cb573a529636e8110199d930859ea9e9d1c3
Joelle  Johnson <joellej221@gmail.com>
2023-11-14 15:23:28 -0500

    connected to db, added signup and update recipe

--- index.ts
+++ index.ts
@@ -11,7 +11,8 @@ const PORT = process.env.PORT
 
 const client = new MongoClient(process.env.MONGO_URI as string)
 const db = client.db('recipe-app')
-const coll = db.collection('Recipes')
+const recipe = db.collection('Recipes')
+const users = db.collection('Users')
 
 
 app.listen(PORT , () => console.log('Database listening on port', PORT))
@@ -20,8 +21,16 @@ app.get('/', (req, res) => {
   res.json('Here is my API responding')
 })
 
+//signup
+app.post('/signup', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  const newUser = await users.insertOne( { firstName: firstName, lastName: lastName, email: email, password: password })
+  res.status(201).send(newUser)
+})
+
+//update recipe
 app.patch('/:_id', async (req, res) => {
   const cleanId = new ObjectId(req.params._id)
-  const updatedRecipe = await coll.findOneAndUpdate( {_id: cleanId}, {$set: req.body})
+  const updatedRecipe = await recipe.findOneAndUpdate( {_id: cleanId}, {$set: req.body})
   res.send(updatedRecipe)
 })
