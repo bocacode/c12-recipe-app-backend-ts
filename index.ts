5394e7089202ca016fa099a9f5e5334cb6242418
Joelle  Johnson <joellej221@gmail.com>
2023-11-14 15:03:23 -0500

    Update api point using params + req.body, no auth

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
 
 const app = express()
@@ -9,8 +9,19 @@ app.use(express.json())
 
 const PORT = process.env.PORT
 
+const client = new MongoClient(process.env.MONGO_URI as string)
+const db = client.db('recipe-app')
+const coll = db.collection('Recipes')
+
+
 app.listen(PORT , () => console.log('Database listening on port', PORT))
 
 app.get('/', (req, res) => {
   res.json('Here is my API responding')
+})
+
+app.patch('/:_id', async (req, res) => {
+  const cleanId = new ObjectId(req.params._id)
+  const updatedRecipe = await coll.findOneAndUpdate( {_id: cleanId}, {$set: req.body})
+  res.send(updatedRecipe)
 })
